4d8498e67122230dd406681438b9137dc9bf9b1f
gabrielguglik <gabrielguglielmik@gmail.com>
2025-06-21 22:54:26 -0300

    feat: adds getAuthHeaders function

---
+++ scripts/getAuthHeaders.js
@@ -0,0 +1,9 @@
+export function getAuthHeaders(username = 'admin', password = 'admin') {
+    const credentials = `${username}:${password}`;
+    const encodedCredentials = encoding.b64encode(credentials);
+  
+    return {
+      Authorization: `Basic ${encodedCredentials}`,
+      'Content-Type': 'application/json',
+    };
+  }
