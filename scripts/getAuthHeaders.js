5d5375db2cb9576b14ab0a79ada64b6167ce558a
gabrielguglik <gabrielguglielmik@gmail.com>
2025-09-14 15:09:35 -0300

    refactor: changes in the structure

--- scripts/getAuthHeaders.js
+++
@@ -1,9 +0,0 @@
-export function getAuthHeaders(username = 'admin', password = 'admin') {
-    const credentials = `${username}:${password}`;
-    const encodedCredentials = encoding.b64encode(credentials);
-  
-    return {
-      Authorization: `Basic ${encodedCredentials}`,
-      'Content-Type': 'application/json',
-    };
-  }
