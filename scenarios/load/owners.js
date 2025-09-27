c96619639a4ff4ecdb0bda46dfafcbf20630af61
gabrielguglik <gabrielguglielmik@gmail.com>
2025-09-27 12:25:56 -0300

    test: adds missing scenarios and improves checks

--- scenarios/load/owners.js
+++ scenarios/load/owners.js
@@ -29,26 +29,26 @@ export default function () {
         };
     
         check(res, {
-            'Status code is 200': (r) => r.status === 200,
-            'Body response is not empty': (r) => r.body.length > 0,
-            'Request duration is less than 200ms': (r) => r.timings.duration < 200
+            'Retrieved all pet owners with success (200)': (r) => r.status === 200,
+            '01. Body response is not empty': (r) => r.body.length > 0,
+            '01. Request duration is less than 200ms': (r) => r.timings.duration < 200
         });
     
         sleep(1);
     });
 
     group('02. Get a pet owner by ID', function () {
-        const res = http.get(`${url}/2`, { headers: authHeaders });
+        const res = http.get(`${url}/1`, { headers: authHeaders });
     
         if (res.status !== 200) {
             console.error(`${url} Failed. Status: ${res.status}`);
         };
     
         check(res, {
-            'Status code is 200': (r) => r.status === 200,
-            'Body response is not empty': (r) => r.body.length > 0,
-            'Owner id is correct': (r) => r.json().id === 1,
-            'Request duration is less than 150ms': (r) => r.timings.duration < 150
+            'Got a pet owner by ID with success (200)': (r) => r.status === 200,
+            '02. Body response is not empty': (r) => r.body.length > 0,
+            '02. Owner id is correct': (r) => r.json().id === 1,
+            '02. Request duration is less than 150ms': (r) => r.timings.duration < 150
         });
     
         sleep(1);
@@ -70,9 +70,9 @@ export default function () {
         };
     
         check(res, {
-            'Status code is 201': (r) => r.status === 201,
-            'Body response is not empty': (r) => r.body.length > 0,
-            'Request duration is less than 200ms': (r) => r.timings.duration < 200
+            'Added a new pet owner with success (201)': (r) => r.status === 201,
+            '03. Body response is not empty': (r) => r.body.length > 0,
+            '03. Request duration is less than 200ms': (r) => r.timings.duration < 200
         });
     
         sleep(1);
@@ -87,22 +87,22 @@ export default function () {
             "telephone": "6085551023"
         });
 
-        const res = http.put(`${url}/2`, putPayload, { headers: authHeaders });
+        const res = http.put(`${url}/1`, putPayload, { headers: authHeaders });
     
         if (res.status !== 204) {
             console.error(`${url} Failed. Status: ${res.status}`);
         };
     
         check(res, {
-            'Status code is 204': (r) => r.status === 204,
-            'Request duration is less than 150ms': (r) => r.timings.duration < 150
+            'Updated an owner’s details with success (204)': (r) => r.status === 204,
+            '04. Request duration is less than 200ms': (r) => r.timings.duration < 200
         });
     
         sleep(1);
     });
 
     // group('05. Delete an owner', function () {
-    //     const ownerId = 1;
+    //     let ownerId = 1;
 
     //     const res = http.delete(`${url}/${ownerId}`, { headers: authHeaders });
     
@@ -111,13 +111,101 @@ export default function () {
     //     };
     
     //     check(res, {
-    //         'Status code is 204': (r) => r.status === 204,
-    //         'Request duration is less than 150ms': (r) => r.timings.duration < 150
+    //         'Deleted an owner with success (204)': (r) => r.status === 204,
+    //         '05. Request duration is less than 150ms': (r) => r.timings.duration < 150
     //     });
 
     //     ownerId++;
     
     //     sleep(1);
     // });
+
+    group('06. Get a pet by ID (owner’s pet)', function () {
+        const res = http.get(`${url}/1/pets/1`, { headers: authHeaders });
+    
+        if (res.status !== 200) {
+            console.error(`${url} Failed. Status: ${res.status}`);
+        };
+    
+        check(res, {
+            'Got a pet by ID (owner’s pet) with success (200)': (r) => r.status === 200,
+            '06. Body response is not empty': (r) => r.body.length > 0,
+            '06. Owner id is correct': (r) => r.json().ownerId === 1,
+            '06. Pet id is correct': (r) => r.json().id === 1,
+            '06. Request duration is less than 150ms': (r) => r.timings.duration < 150
+        });
+    
+        sleep(1);
+    });
+
+    group('07. Update pet details (owner’s pet)', function () {
+        const putPayload = JSON.stringify({
+            "name": "Leo",
+            "birthDate": "2010-09-07",
+            "type": {
+                "name": "cat",
+                "id": 1
+            }
+        });
+
+        const res = http.put(`${url}/1/pets/1`, putPayload, { headers: authHeaders });
+    
+        if (res.status !== 204) {
+            console.error(`${url} Failed. Status: ${res.status}`);
+        };
+    
+        check(res, {
+            'Updated pet details (owner’s pet) with success (204)': (r) => r.status === 204,
+            '07. Request duration is less than 200ms': (r) => r.timings.duration < 200
+        });
+    
+        sleep(1);
+    });
+
+    group('08. Add a new pet to an owner', function () {
+        const postPayload = JSON.stringify({
+            "name": "Leo",
+            "birthDate": "2010-09-07",
+            "type": {
+                "name": "cat",
+                "id": 1
+            }
+        });
+
+        const res = http.post(`${url}/1/pets`, postPayload, { headers: authHeaders });
+    
+        if (res.status !== 201) {
+            console.error(`${url} Failed. Status: ${res.status}`);
+        };
+    
+        check(res, {
+            'Added a new pet to an owner with success (201)': (r) => r.status === 201,
+            '08. Body response is not empty': (r) => r.body.length > 0,
+            '08. Request duration is less than 200ms': (r) => r.timings.duration < 200
+        });
+    
+        sleep(1);
+    });
+
+    group('09. Add a vet visit for a pet', function () {
+        const postPayload = JSON.stringify({
+            "date": "2030-01-01",
+            "description": "rabies shot"
+        });
+
+        const res = http.post(`${url}/1/pets/1/visits`, postPayload, { headers: authHeaders });
+    
+        if (res.status !== 201) {
+            console.error(`${url} Failed. Status: ${res.status}`);
+        };
+    
+        check(res, {
+            'Added a vet visit for a pet with success (201)': (r) => r.status === 201,
+            '09. Body response is not empty': (r) => r.body.length > 0,
+            '09. Request duration is less than 200ms': (r) => r.timings.duration < 200
+        });
+    
+        sleep(1);
+    });
 
 };
